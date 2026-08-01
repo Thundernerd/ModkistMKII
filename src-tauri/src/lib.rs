18ddc8a1d70e4102d2ca72d13f586a6e43e7a9f7
Thundernerd <github@tnrd.net>
2026-08-01 13:28:16 +0200

    chore: remove unused greet command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -91,11 +91,6 @@ const RELOAD_IF_BLANK_JS: &str = r#"window.setTimeout(function () {
   }
 }, 500)"#;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
